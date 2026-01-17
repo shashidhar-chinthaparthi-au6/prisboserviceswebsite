a6cc2f9052f2f29eab5167dd258a33e8fee045a2
shashidhar-chinthaparthi-au6 <58417872+shashidhar-chinthaparthi-au6@users.noreply.github.com>
2026-01-17 07:17:50 +0530

    Add Google Analytics (gtag.js) tracking to all pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-9BXRZR82DG"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-9BXRZR82DG');
+            `,
+          }}
+        />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{
